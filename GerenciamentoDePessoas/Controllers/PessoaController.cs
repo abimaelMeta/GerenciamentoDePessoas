390b2f7286d0a6851779dadd69a1fdd1301aa52a
abimaelMeta <abimael.mendes@metanetsistemas.com.br>
2024-12-26 19:02:53 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoDePessoas/Controllers/PessoaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciamentoDePessoas.Controllers
+{
+    [Route("Usuario")]
+    public class PessoaController : Controller
+    {
+        [Route("Inicio")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("Detalhes/{id:int}")]
+        public IActionResult DetalhesUsuario(int id)
+        {
+            return View();
+        }
+    }
+}
