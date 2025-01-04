c0460c3cdcb185a584096c738555922bde8b1fed
abimaelMeta <abimael.mendes@metanetsistemas.com.br>
2025-01-04 10:45:17 -0300

    Passagem de dados: ViewBag, ViewData, TempData

--- GerenciamentoDePessoas/Controllers/PessoaController.cs
+++ GerenciamentoDePessoas/Controllers/PessoaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GerenciamentoDePessoas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GerenciamentoDePessoas.Controllers
 {
@@ -8,13 +9,22 @@ namespace GerenciamentoDePessoas.Controllers
         [Route("Inicio")]
         public IActionResult Index()
         {
+            ViewBag.Mensagem = TempData["TextoParaAIndex"];
             return View();
         }
         
         [Route("Detalhes/{id:int}")]
         public IActionResult DetalhesUsuario(int id)
         {
-            return View();
+            //ViewBag.TextoTelaDetalhes = "Texto para a tela de detalhes.";
+            //ViewBag.DataAtual = DateTime.Now;
+
+            //ViewData["TextoTelaDetalhes"] = "Texto para a tela de detalhes.";
+            //ViewData["DataAtual"] = DateTime.Now;
+
+            TempData["TextoParaAIndex"] = "Texto que vai aparecer na Index de Pessoa.";
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ GerenciamentoDePessoas/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace GerenciamentoDePessoas.Models
+{
+    public class Pessoa
+    {
+        public Pessoa(int id, string nome, string sobrenome, DateTime dataNascimento)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = dataNascimento;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
