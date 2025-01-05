8cd5a7d2a7975083ef6080c855220951091c6294
abimaelMeta <abimael.mendes@metanetsistemas.com.br>
2025-01-05 17:17:22 -0300

    Geração de Controllers e Views automaticamente.(Scaffold)

--- GerenciamentoDePessoas/Program.cs
+++ GerenciamentoDePessoas/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GerenciamentoDePessoas.Data;
 namespace GerenciamentoDePessoas
 {
     public class Program
@@ -5,6 +8,8 @@ namespace GerenciamentoDePessoas
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<GerenciamentoDePessoasContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("GerenciamentoDePessoasContext") ?? throw new InvalidOperationException("Connection string 'GerenciamentoDePessoasContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
